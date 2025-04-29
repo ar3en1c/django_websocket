f661a7e5d6907945bbe8d599aa9cd1bd09fe197d
Erfan <ar3en1c@outlook.com>
2025-04-29 12:28:58 +0330

    complete 3rd stage

--- django_chatbot/asgi.py
+++ django_chatbot/asgi.py
@@ -1,16 +1,16 @@
 import os
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import chat.routing
+from channels.auth import AuthMiddlewareStack
+import chat.routing  # 👈 مسیر اپلیکیشن خودت
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_chatbot.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            chat.routing.websocket_urlpatterns
+            chat.routing.websocket_urlpatterns  # 👈 می‌سازیمش الان
         )
     ),
 })
